feb4fd532719ed194286db64fbecc53668baf047
Axel <axelehrlich8@gmail.com>
2021-06-21 00:54:45 +0200

    javascript logic work on comprehension

--- jstest.js
+++ jstest.js
@@ -4,4 +4,17 @@
 function newElement() {
     var li = document.createElement("li");
     var inputValue = document.getElementById("myInput").value;
-    var t = document.createTextNode(inputValue); }
+    var t = document.createTextNode(inputValue); 
+    li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = ""; 
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span); }
