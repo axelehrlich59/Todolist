dcc1df94256855b9b50ef7d9238a856ea5f0302e
Axel <axelehrlich8@gmail.com>
2021-06-20 16:38:40 +0200

    first commit

---
+++ jstest.js
@@ -0,0 +1,7 @@
+// This is a personnal file in js to test my proper code and evaluate the possibilites 
+
+
+function newElement() {
+    var li = document.createElement("li");
+    var inputValue = document.getElementById("myInput").value;
+    var t = document.createTextNode(inputValue); }
